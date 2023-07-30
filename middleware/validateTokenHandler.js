cbbf2e16eb280c43a5ab8efa3de569de1daae2e9
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2023-07-30 12:14:44 +0300

    Update validateTokenHandler.js
    This way the code will first check for the presence of the token and respond with an error if it is missing or not properly formatted. If the token is present and valid, it will verify and decode, set req.user, and call next(). Otherwise, it will throw an error and send a 401 status response.

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -2,24 +2,24 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
-  let token;
-  let authHeader = req.headers.Authorization || req.headers.authorization;
-  if (authHeader && authHeader.startsWith("Bearer")) {
+    let token;
+    let authHeader = req.headers.Authorization || req.headers.authorization;
+  
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      res.status(401);
+      throw new Error("User not authorized or token missing");
+    }
+  
     token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, decoded) => {
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
         res.status(401);
-        throw new Error("User is not authorized");
+        throw new Error("User not authorized!");
       }
       req.user = decoded.user;
       next();
     });
-
-    if (!token) {
-      res.status(401);
-      throw new Error("User is not authorized or token is missing");
-    }
-  }
-});
+  });
 
 module.exports = validateToken;
